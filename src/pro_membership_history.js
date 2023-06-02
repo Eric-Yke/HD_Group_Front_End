5c81ae6f4930e73afb3615acd70b9bee0775e6d3
Eric <cg836@uowmail.edu.au>
2023-06-02 11:35:42 +1000

    ver 1.0.1

--- src/pro_membership_history.js
+++ src/pro_membership_history.js
@@ -2,6 +2,35 @@ $(document).ready(function () {
 
   //导入json数据
   $.ajax({
+    url: 'pro_payment.json',
+    dataType: 'json',
+    success: function (data) {
+      // 将 JSON 数据填充到页面中
+      const table = document.getElementById("table1");
+      const tbody = table.getElementsByTagName("tbody")[0];
+      data.forEach(function (item) {
+        const row = tbody.insertRow();
+
+        const id_cell = row.insertCell();
+        const date_cell = row.insertCell();
+        const serviceType_cell = row.insertCell();
+        const revenue_cell = row.insertCell();
+        const ostatus_cell = row.insertCell();
+
+        id_cell.innerHTML = item.order_id;
+        date_cell.innerHTML = item.order_date;
+        serviceType_cell.innerHTML = item.service_requested;
+        revenue_cell.innerHTML = item.revenue;
+        ostatus_cell.innerHTML = item.order_status;
+      });
+    },
+    error: function () {
+      console.log('Oops, Something going wrong...');
+    }
+  });
+  
+  //导入json数据
+  $.ajax({
     url: 'pro_membership_history.json',
     dataType: 'json',
     success: function (data) {
@@ -26,4 +55,5 @@ $(document).ready(function () {
       console.log('Oops, Something going wrong...');
     }
   });
+
 });
